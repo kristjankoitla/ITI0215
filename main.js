46ccf77e6ca07828617c9657f49f5d9b55d8d4ee
Kristjan Koitla <koitlakristjan@gmail.com>
2022-02-05 20:25:38 +0200

    Ex 2

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 const http = require("http");
 
-const host = "localhost";
-const port = 8000;
+const HOST = process.env.HOST ? process.env.HOST : "0.0.0.0";
+const PORT = process.env.PORT ? process.env.PORT : 80;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+const requestListener = async (req, res) => {
+    const decodedUrl = decodeURI(req.url);
+    console.log(`${req.method} ${decodedUrl}`);
+
+    if (!["GET", "POST"].includes(req.method)) {
+        res.writeHead(405, "Method Not Allowed");
+        res.end();
+        return;
+    }
+
+    if (req.method === "GET") {
+        res.end(decodedUrl);
+        return;
+    }
+
+    if (req.method === "POST") {
+        const data = await getData(req);
+        res.end(data);
+        return;
+    }
 };
 
+async function getData(req) {
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    const data = Buffer.concat(buffers).toString();
+    return data;
+}
+
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
